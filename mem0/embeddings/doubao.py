17ecfd172a11911c7247f5eeae1d9f3991c6e6b2
JR <815732072@qq.com>
2025-07-29 21:45:54 +0800

    Add doubao apis

---
+++ mem0/embeddings/doubao.py
@@ -0,0 +1,32 @@
+import os
+import warnings
+from typing import Literal, Optional
+
+from volcenginesdkarkruntime import Ark
+
+from mem0.configs.embeddings.base import BaseEmbedderConfig
+from mem0.embeddings.base import EmbeddingBase
+
+
+class DoubaoEmbedding(EmbeddingBase):
+    def __init__(self, config: Optional[BaseEmbedderConfig] = None):
+        super().__init__(config)
+        self.config.model = self.config.model or "doubao-embedding-text-240715"
+        self.client = Ark(api_key=api_key)
+
+    def embed(self, text, memory_action: Optional[Literal["add", "search", "update"]] = None):
+        """
+        Get the embedding for the given text using OpenAI.
+
+        Args:
+            text (str): The text to embed.
+            memory_action (optional): The type of embedding to use. Must be one of "add", "search", or "update". Defaults to None.
+        Returns:
+            list: The embedding vector.
+        """
+        text = text.replace("\n", " ")
+        return (
+            self.client.embeddings.create(input=[text], model=self.config.model, encoding_format="float")
+            .data[0]
+            .embedding
+        )
