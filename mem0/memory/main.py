5f966c1a2ec9d7da11bcec973e57bf25a14f78bb
JR <815732072@qq.com>
2025-08-05 02:13:32 +0800

    Support custum createed_date

--- mem0/memory/main.py
+++ mem0/memory/main.py
@@ -812,7 +812,7 @@ class Memory(MemoryBase):
         metadata = metadata or {}
         metadata["data"] = data
         metadata["hash"] = hashlib.md5(data.encode()).hexdigest()
-        metadata["created_at"] = datetime.now(pytz.timezone("US/Pacific")).isoformat()
+        metadata["created_at"] = metadata.get("created_at", None) or datetime.now(pytz.timezone("US/Pacific")).isoformat()
 
         self.vector_store.insert(
             vectors=[embeddings],
